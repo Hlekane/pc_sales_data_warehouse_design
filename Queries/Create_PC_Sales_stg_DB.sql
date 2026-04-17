99757f35b1c0379e0d2ef8ad7b6fc4e03391bd67
Hlekane <pertuniangobeni2@gmail.com>
2026-04-17 11:29:37 +0200

    Apply best practice for database creation, update syntax

--- Queries/Create_PC_Sales_stg_DB.sql
+++ Queries/Create_PC_Sales_stg_DB.sql
@@ -1,2 +1,16 @@
--- Create Database
-CREATE DATABASE PC_Sales_Stg; 
+-- Create staging database
+
+IF DB_ID('PC_Sales_Stg') IS NOT NULL
+BEGIN
+    CREATE DATABASE PC_Sales_DW;
+    
+END
+GO
+
+-- Create dataware house database
+IF DB_ID('PC_Sal_Dwh') IS NOT NULL
+BEGIN
+    CREATE DATABASE PC_Sales_Dwh;
+    
+END
+GO
